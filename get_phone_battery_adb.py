d20c39739e45f631233cf789e0cc50af318e4724
diam0ndkiller <>
2025-12-30 12:57:50 +0100

    clean up backend

--- get_phone_battery_adb.py
+++ get_phone_battery_adb.py
@@ -4,12 +4,12 @@ import subprocess
 
 with open("phone_ips.txt", "r") as file: phone_ips = file.read().split("\n")
 
-o=""
+result=""
 for ip in phone_ips:
 	try:
 		if ip != "" and not "failed" in subprocess.check_output(f"adb connect {ip}:5542", shell=True).decode("utf-8"):
-			o = subprocess.check_output(f"echo {ip} && adb -s {ip}:5542 shell dumpsys battery", shell=True).decode("utf-8")
+			result = subprocess.check_output(f"echo {ip} && adb -s {ip}:5542 shell dumpsys battery", shell=True).decode("utf-8")
 			break
 	except: continue
-print(o)
-with open("battery_data.txt", "w") as file: file.write(o)
+
+print(result)
